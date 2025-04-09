a3f2b4581aca01e854b353bcf041e22f0d9312f3
lukvmil <millerluke1364@gmail.com>
2025-04-09 17:11:05 +0800

    set up async requests and backfill on server run, still has issues with rate limiting

--- hackmd_sensor_node/hackmd_api.py
+++ hackmd_sensor_node/hackmd_api.py
@@ -1,26 +1,46 @@
-import requests, time
+import asyncio
+import httpx
 from .config import HACKMD_API_TOKEN
 
 api_base_url = "https://api.hackmd.io/v1"
 
 def request(path, method="GET"):
+    resp = httpx.request(
+        method=method,
+        url=api_base_url+path,
+        headers={
+            "Authorization": "Bearer " + HACKMD_API_TOKEN
+        }
+    )
+
+    if resp.status_code == 200:
+        return resp.json()
+    
+    else:
+        print(resp.status_code, resp.text)
+        return
+
+async def async_request(path, method="GET"):
     timeout = 60
     
     while True: 
-        resp = requests.request(
-            method = method,
-            url = api_base_url + path, 
-            headers = {
-                "Authorization": "Bearer " + HACKMD_API_TOKEN
-            }
-        )
+        async with httpx.AsyncClient() as client:
+            
+            resp = await client.request(
+                method=method,
+                url=api_base_url+path,
+                headers={
+                    "Authorization": "Bearer " + HACKMD_API_TOKEN
+                }
+            )
+        
 
         if resp.status_code == 200:
             return resp.json()
 
         elif resp.status_code == 429:
             print(resp.status_code, resp.text, f"retrying in {timeout} seconds")
-            time.sleep(timeout)
+            await asyncio.sleep(timeout)
             timeout *= 2
         else:
             print(resp.status_code, resp.text)
