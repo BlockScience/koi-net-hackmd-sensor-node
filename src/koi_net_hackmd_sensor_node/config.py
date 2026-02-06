7f4688ff800a6575d5c57038859bf97f81241763
lukvmil <millerluke1364@gmail.com>
2026-02-06 13:02:42 -0500

    fixed merge conflicts

--- src/koi_net_hackmd_sensor_node/config.py
+++ src/koi_net_hackmd_sensor_node/config.py
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-from pydantic import BaseModel
-from koi_net.config.full_node import FullNodeConfig, KoiNetConfig, NodeProfile, NodeProvides
-from koi_net.config.core import EnvConfig
-from rid_lib.types import HackMDNote
-
-
-class HackMDConfig(BaseModel):
-    team_path: str = "blockscience"
-
-class HackMDEnvConfig(EnvConfig):
-    hackmd_api_token: str = "HACKMD_API_TOKEN"
-
-class HackMDSensorNodeConfig(FullNodeConfig):
-    koi_net: KoiNetConfig = KoiNetConfig(
-        node_name="hackmd-sensor",
-        node_profile=NodeProfile(
-            provides=NodeProvides(
-                event=[HackMDNote],
-                state=[HackMDNote]
-            )
-        )
-    )
-    env: HackMDEnvConfig = HackMDEnvConfig()
-    hackmd: HackMDConfig = HackMDConfig()
-=======
 from pydantic import Field, BaseModel, PrivateAttr
 from koi_net_shared import HackMDNote
 from koi_net.config.full_node import (
@@ -76,4 +50,3 @@ class HackMDSensorConfig(FullNodeConfig):
         ),
         rid_types_of_interest=[KoiNetNode]
     )
->>>>>>> dev
